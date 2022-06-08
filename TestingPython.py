cb3a85c42a12352dcda5e6197458dbb68af48b92
telefoose <alanlbergman@gmail.com>
2022-06-08 17:27:33 -0300

    Test 1

--- TestingPython.py
+++ TestingPython.py
@@ -1,2 +1,11 @@
-print("new world")
-Print("New Pull request")
+# My Jupyter Notebook on IBM Watson Studio
+**Alan L**
+Data Scientist
+*I am interested in data science because i am a tech savvy person and love to build computers*
+### My code shows hello world on the screen
+print("hello world")
+_________________
+
+[link](https://www.twitch.tv/)
+
+![The San Juan Mountains are beautiful!](/assets/images/san-juan-mountains.jpg "San Juan Mountains")
